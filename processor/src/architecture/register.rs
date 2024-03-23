366295df9436e5fd411fb0ab8d0666e574c4b56d
Lannee <3lirris@gmail.com>
2024-03-24 01:37:23 +0300

    Changed concept of register struct

--- processor/src/architecture/register.rs
+++ processor/src/architecture/register.rs
@@ -1,12 +1,20 @@
+pub static REG0: Register32 = Register32::new(); 
+pub static REG1: Register32 = Register32::new(); 
+pub static REG2: Register32 = Register32::new(); 
+pub static REG3: Register32 = Register32::new(); 
+pub static REG4: Register32 = Register32::new(); 
+pub static REG5: Register32 = Register32::new(); 
+pub static REG6: Register32 = Register32::new(); 
+pub static REG7: Register32 = Register32::new(); 
 
+pub type Register32 = Register<i32>;
 
-pub enum Register {
-    REG0,
-    REG1,
-    REG2,
-    REG3,
-    REG4,
-    REG5,
-    REG6,
-    REG7,
+struct Register<T: Copy> {
+    value: T
+}
+
+impl<T: Copy + Default> Register<T> {
+    fn new() -> Register<T> {
+        Register { value: T::default() }
+    }
 }
