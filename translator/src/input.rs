dd0f43d6fa63992b2c002c8dc2a0e656b5bbece2
Lannee <3lirris@gmail.com>
2024-03-25 15:31:00 +0300

    Added get_target_file method, changed realization of input methods

--- translator/src/input.rs
+++ translator/src/input.rs
@@ -1,17 +1,41 @@
 pub mod env_args;
 
+use std::fs::File;
+
 use crate::errors::TranslatorInputError;
 
+pub type SourceCode = String;
+
 pub fn get_src() -> Result<SourceCode, TranslatorInputError> {
+    std::fs::read_to_string(
+        get_src_path()?
+    ).map_err(|err| TranslatorInputError::FileError(err))
+}
+
+pub fn get_target_file() -> Result<File, TranslatorInputError> {
+    let env = env_args::EnvArgs::get();
+
+    match env.out_file {
+        None => File::create(file_name_as_json(&get_src_path()?)),
+        Some(target_path) => File::open(target_path)
+    }
+    .map_err(|err| TranslatorInputError::FileError(err))
+}
+
+fn get_src_path() -> Result<String, TranslatorInputError> {
     let env = env_args::EnvArgs::get();
     match env.source_file {
         None => Err(TranslatorInputError::NoInputFile),
-        Some(src) => {
-            Ok(std::fs::read_to_string(src)
-                .map_err(|err| TranslatorInputError::FileError(err))?)
-        }  
+        Some(src) => Ok(src) 
     }
 }
 
+fn file_name_as_json(file_name: &String) -> String {
+    let mut name = match file_name.rsplit_once(".") {
+        None => file_name.clone(),
+        Some(name) => name.0.to_string()
+    };
 
-pub type SourceCode = String;
+    name.push_str(".json");
+    name
+}
