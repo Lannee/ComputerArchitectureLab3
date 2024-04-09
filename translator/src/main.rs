ef73d97abb65ff7252e580f02c3d1edd69eb1b83
Lannee <3lirris@gmail.com>
2024-04-09 21:28:26 +0300

    added covering method above translation proccess

--- translator/src/main.rs
+++ translator/src/main.rs
@@ -8,13 +8,13 @@ use std::io::Write;
 
 use errors::TranslationError;
 use input::SourceCode;
-use translator::parser::parse;
+use translator::translate;
 
 fn main() -> Result<(), TranslationError> {
     let code: SourceCode = input::get_src()
         .map_err(|err| TranslationError::InputError(err))?;
 
-    let machine_code = parse(code)?;
+    let machine_code = translate(code)?;
 
     input::get_target_file()
         .map_err(|err| TranslationError::InputError(err))?
--- translator/src/translator.rs
+++ translator/src/translator.rs
@@ -1,2 +1,9 @@
+use crate::{errors::TranslationError, input::SourceCode, machine_code::MachineCode};
+
 pub mod parser;
 mod format;
+
+
+pub fn translate(code: SourceCode) -> Result<MachineCode, TranslationError> {
+    todo!()
+}
