413aba685d00d2634b57e1823e4bc876c72e8277
Lannee <3lirris@gmail.com>
2024-04-14 16:15:32 +0300

    changed processor registers

--- processor/src/architecture/register.rs
+++ processor/src/architecture/register.rs
@@ -1,15 +1,17 @@
-pub static REG0: Register32 = Register32::new(); 
-pub static REG1: Register32 = Register32::new(); 
-pub static REG2: Register32 = Register32::new(); 
-pub static REG3: Register32 = Register32::new(); 
-pub static REG4: Register32 = Register32::new(); 
-pub static REG5: Register32 = Register32::new(); 
-pub static REG6: Register32 = Register32::new(); 
-pub static REG7: Register32 = Register32::new(); 
+use crate::define_register32;
 
 pub type Register32 = Register<i32>;
 
-struct Register<T: Copy> {
+define_register32!{REG0}
+define_register32!{REG1}
+define_register32!{REG2}
+define_register32!{REG3}
+define_register32!{REG4}
+define_register32!{REG5}
+define_register32!{REG6}
+define_register32!{REG7}
+
+pub struct Register<T: Copy> {
     value: T
 }
 
@@ -17,4 +19,21 @@ impl<T: Copy + Default> Register<T> {
     fn new() -> Register<T> {
         Register { value: T::default() }
     }
+
+    fn get_value(&self) -> T {
+        self.value
+    }
+
+    fn set_value(&mut self, value: T) {
+        self.value = value;
+    }
+}
+
+#[macro_export]
+macro_rules! define_register32 {
+    ($name:ident) => {
+        pub static mut $name: Register32 = Register32 {
+            value: 0 as i32
+        };
+    };
 }
