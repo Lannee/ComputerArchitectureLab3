17549bb4b6bee9993c400695bff23d3ae7425f09
Lannee <3lirris@gmail.com>
2024-04-15 00:57:42 +0300

    delated files

--- processor/src/programs/commands.rs
+++
@@ -1,9 +0,0 @@
-use crate::architecture::register::Register;
-
-pub enum Command {
-    MovRegReg(Register, Register),
-    MovRegVal(Register, i32),
-
-    In(Register, Register),
-    Out(Register, Register),
-}
